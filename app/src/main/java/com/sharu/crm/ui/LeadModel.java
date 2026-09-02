5fb45ffdbbd4c467e50b4d6a39bc501fea8dc7a4
jimkabharath <jimkabharath@gmail.com>
2026-09-02 16:31:30 +0530

    Change LeadModel fields to public access
    Changed member variables from private to public access.

--- app/src/main/java/com/sharu/crm/ui/LeadModel.java
+++ app/src/main/java/com/sharu/crm/ui/LeadModel.java
@@ -1,10 +1,10 @@
 package com.sharu.crm.ui;
 
 public class LeadModel {
-    private String name;
-    private String phone;
-    private String status;
-    private String details;
+    public String name;
+    public String phone;
+    public String status;
+    public String details;
 
     public LeadModel(String name, String phone, String status, String details) {
         this.name = name;
